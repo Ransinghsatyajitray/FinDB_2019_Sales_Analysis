7042d85b79674bcd550ccc5d5b57944ad860ffe4
swtzang <swtzang@example.com>
2019-05-29 17:04:20 +0800

    0529

--- time_series/checkpoint_3_forcats.R
+++ time_series/checkpoint_3_forcats.R
@@ -4,7 +4,7 @@
 library(tidyverse)
 library(tidyquant)
 
-bike_orderlines_tbl <- read_rds("00_data/bike_sales/data_wrangled/bike_orderlines.rds")
+bike_orderlines_tbl <- read_rds("../sales_analysis/data_wrangled_student/bike_orderlines.rds")
 
 bike_orderlines_tbl
 
